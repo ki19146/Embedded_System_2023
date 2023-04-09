e37f0547c510e98dac4acc782337500c9a8276b7
Yiyao Wang <78681139+JessicaWangCode@users.noreply.github.com>
2023-04-09 10:40:18 +0100

    Add files via upload
    Task2 and Task3 done

--- main_gtz.c
+++ main_gtz.c
@@ -1,6 +1,6 @@
 /*
  *  ======== gtz.c ========
- */    
+ */
 
 #include <xdc/std.h>
 #include <xdc/runtime/System.h>
@@ -9,93 +9,119 @@
 #include <ti/sysbios/knl/Clock.h>
 #include <ti/sysbios/knl/Task.h>
 
+#include <xdc/runtime/Types.h>
+#include <xdc/runtime/Timestamp.h>
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
 #include "gtz.h"
 
-void clk_SWI_Generate_DTMF(UArg arg0);
-void clk_SWI_GTZ_0697Hz(UArg arg0);
+void clk_SWI_Read_Data(UArg arg0);
+void clk_SWI_GTZ_All_Freq(UArg arg0);
 
 extern void task0_dtmfGen(void);
 extern void task1_dtmfDetect(void);
 
-extern char digit;
-extern int sample, mag1, mag2, freq1, freq2, gtz_out[8];
+extern int sample, tdiff, tdiff_final, gtz_out[8];
 extern short coef[8];
+extern int flag;
 
-
+short data[NO_OF_SAMPLES];
+short *buffer;
 
 /*
  *  ======== main ========
  */
-void main(void)
-{
-
-
-	System_printf("\n I am in main :\n");
+int main() {
+	System_printf("\n System Start\n");
 	System_flush();
+
+	/* Read binary data file */
+	FILE* fp = fopen("../data.bin", "rb");
+	if(fp==0) {
+		System_printf("Error: Data file not found\n");
+		System_flush();
+		return 1;
+	}
+	fread(data, 2, NO_OF_SAMPLES, fp);
+	buffer = (short*)malloc(2*8*10000);
+
 	/* Create a Clock Instance */
-    Clock_Params clkParams;
+	Clock_Params clkParams;
 
-    /* Initialise Clock Instance with time period and timeout (system ticks) */
-    Clock_Params_init(&clkParams);
-    clkParams.period = 1;
-    clkParams.startFlag = TRUE;
+	/* Initialise Clock Instance with time period and timeout (system ticks) */
+	Clock_Params_init(&clkParams);
+	clkParams.period = 1;
+	clkParams.startFlag = TRUE;
 
-    /* Instantiate ISR for tone generation  */
-	Clock_create(clk_SWI_Generate_DTMF, TIMEOUT, &clkParams, NULL);
+	/* Instantiate ISR for tone generation  */
+	Clock_create(clk_SWI_Read_Data, TIMEOUT, &clkParams, NULL);
 
-    /* Instantiate 8 parallel ISRs for each of the eight Goertzel coefficients */
-	Clock_create(clk_SWI_GTZ_0697Hz, TIMEOUT, &clkParams, NULL);
-
-	mag1 = 32768.0; mag2 = 32768.0; freq1 = 697; // I am setting freq1 = 697Hz to test my GTZ algorithm with one frequency.
+	/* Instantiate 8 parallel ISRs for each of the eight Goertzel coefficients */
+	Clock_create(clk_SWI_GTZ_All_Freq, TIMEOUT, &clkParams, NULL);
 
 	/* Start SYS_BIOS */
-    BIOS_start();
+	BIOS_start();
 }
 
 /*
- *  ====== clk0Fxn =====
+ *  ====== clk_SWI_Generate_DTMF =====
  *  Dual-Tone Generation
- *  ====================
+ *  ==================================
  */
-void clk_SWI_Generate_DTMF(UArg arg0)
-{
+void clk_SWI_Read_Data(UArg arg0) {
 	static int tick;
-
-
 	tick = Clock_getTicks();
-
-//	sample = (int) 32768.0*sin(2.0*PI*freq1*TICK_PERIOD*tick) + 32768.0*sin(2.0*PI*freq2*TICK_PERIOD*tick);
-	sample = (int) 32768.0*sin(2.0*PI*freq1*TICK_PERIOD*tick) + 32768.0*sin(2.0*PI*0*TICK_PERIOD*tick);
-	sample = sample >>12;
+	sample = data[tick%NO_OF_SAMPLES];
 }
 
 /*
  *  ====== clk_SWI_GTZ =====
  *  gtz sweep
- *  ====================
+ *  ========================
  */
-void clk_SWI_GTZ_0697Hz(UArg arg0)
-{
-   	static int N = 0;
-   	static int Goertzel_Value = 0;
+void clk_SWI_GTZ_All_Freq(UArg arg0) {
+	// define variables for timing
+	static int start, stop;
 
-   	static short delay;
-   	static short delay_1 = 0;
-   	static short delay_2 = 0;
+	// define feedback times as N
+	static int N = 0;
 
-   	int prod1, prod2, prod3;
+   	//Record start time
+	start = Timestamp_get32();
 
-   	short input, coef_1;
+	static int Goertzel_Value = 0;
+	short input = (short) (sample);
 
-
-// to be completed
-
-
-    	gtz_out[0] = Goertzel_Value;
+	/* TODO 1. Complete the feedback loop of the GTZ algorithm*/
+	/* ========================= */
 
 
+	/* ========================= */
+	N++;
+
+	//Record stop time
+	stop = Timestamp_get32();
+	//Record elapsed time
+	tdiff = stop-start;
+
+	if (N == 206) {
+	   	//Record start time
+		start = Timestamp_get32();
+
+		/* TODO 2. Complete the feedforward loop of the GTZ algorithm*/
+		/* ========================= */
+
+		/* gtz_out[..] = ... */
+		/* ========================= */
+		flag = 1;
+		N = 0;
+
+		//Record stop time
+		stop = Timestamp_get32();
+		//Record elapsed time
+		tdiff_final = stop-start;
+	}
 }
